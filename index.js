8a1a752ea0452a5d02e659306432afe776722bd1
Rawan <rawnq.sb@gmail.com>
2022-12-21 01:10:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+
+const distanceFromHqInBlocks = (value) => {
+    if(value > 42) {
+       return value - 42;
+    } else {
+       return 42 - value;
+    }
+ }
+ 
+ const distanceFromHqInFeet = (value) => {
+    const result = distanceFromHqInBlocks(value);
+    return result * 264;
+ }
+ 
+ const distanceTravelledInFeet = (start, destination) => {
+    if(start > destination) {
+       return (start - destination) * 264;
+    } else {
+       return (destination - start) * 264;
+    }
+ }
+ 
+ const calculatesFarePrice = (start, destination) => {
+    const distance = distanceTravelledInFeet(start, destination);
+    if(distance <= 400) {
+       return 0;
+    } else if(distance > 400 && distance <= 2000) {
+       return (distance - 400) * 0.02;
+    } else if(distance > 2000 && distance <= 2500) {
+       return 25;
+    } else {
+       return 'cannot travel that far';
+
+    }
+ }
